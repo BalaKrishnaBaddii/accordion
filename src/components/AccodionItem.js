7a950f17d145ce187a01970c82e22224b6d4175b
BalaKrishnaBaddii <bkrishna1114@gmail.com>
2024-06-13 11:04:37 +0530

    Asscodion createdd

--- src/components/AccodionItem.js
+++ src/components/AccodionItem.js
@@ -1,10 +1,16 @@
+import { useState } from "react";
+
 export default function AccodionItem({ num, title, text }) {
+  const [isOpen, setIsOpen] = useState(false);
+  function handleExpand() {
+    setIsOpen(!isOpen);
+  }
   return (
-    <div className="item">
-      <p className="number">{num}</p>
-      <p className="text">{title}</p>
-      <p className="icon">-</p>
-      <p className="content-box">{text}</p>
+    <div className={`item ${isOpen ? "open" : "item"}`} onClick={handleExpand}>
+      <p className="number">{num <= 9 ? `0${num}` : num}</p>
+      <p className="title">{title}</p>
+      <p className="icon">{isOpen ? "-" : "+"}</p>
+      {isOpen ? <p className="content-box">{text}</p> : null}
     </div>
   );
 }
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -3,8 +3,8 @@ import AccodionItem from "./AccodionItem";
 export default function Accordion({ data }) {
   return (
     <div className="accordion">
-      {data.map((el) => (
-        <AccodionItem title={el.title} text={el.text} />
+      {data.map((el, i) => (
+        <AccodionItem title={el.title} text={el.text} num={i + 1} />
       ))}
     </div>
   );
