91f211be5aae58b6287b92b3a53601e1484489a3
BalaKrishnaBaddii <bkrishna1114@gmail.com>
2024-06-13 14:06:28 +0530

    Toggle only one at atime with resusbality

--- src/components/AccodionItem.js
+++ src/components/AccodionItem.js
@@ -1,16 +1,22 @@
-import { useState } from "react";
-
-export default function AccodionItem({ num, title, text }) {
-  const [isOpen, setIsOpen] = useState(false);
+export default function AccodionItem({
+  num,
+  title,
+  text,
+  curOpen,
+  onOpen,
+  children,
+}) {
+  const isOpen = num === curOpen;
   function handleExpand() {
-    setIsOpen(!isOpen);
+    onOpen(isOpen ? null : num);
   }
+
   return (
     <div className={`item ${isOpen ? "open" : "item"}`} onClick={handleExpand}>
       <p className="number">{num <= 9 ? `0${num}` : num}</p>
       <p className="title">{title}</p>
       <p className="icon">{isOpen ? "-" : "+"}</p>
-      {isOpen ? <p className="content-box">{text}</p> : null}
+      {isOpen ? <p className="content-box">{children}</p> : null}
     </div>
   );
 }
--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,11 +1,34 @@
+import { useState } from "react";
 import AccodionItem from "./AccodionItem";
 
 export default function Accordion({ data }) {
+  const [curOpen, setCurOpen] = useState(null);
+
   return (
     <div className="accordion">
       {data.map((el, i) => (
-        <AccodionItem title={el.title} text={el.text} num={i + 1} />
+        <AccodionItem
+          curOpen={curOpen}
+          onOpen={setCurOpen}
+          title={el.title}
+          num={i + 1}
+        >
+          {el.text}
+        </AccodionItem>
       ))}
+      <AccodionItem
+        curOpen={curOpen}
+        onOpen={setCurOpen}
+        title="Open Me"
+        num={10}
+      >
+        <p>Daily Goals</p>
+        <ul>
+          <li>Wake up at 5 A.M</li>
+          <li>Learn React</li>
+          <li>Apply Jobs 😍</li>
+        </ul>
+      </AccodionItem>
     </div>
   );
 }
